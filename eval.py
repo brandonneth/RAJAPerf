3569240d8300c9fdc89064e453788bc28b1ff1d9
brandonneth <nethbrandone@gmail.com>
2020-08-10 17:42:26 -0700

    updated eval script

--- eval.py
+++ eval.py
@@ -10,7 +10,22 @@ def stencil_eval():
 	prefix = 'Stencils'
 
 	passes_option = '--npasses ' + str(num_passes)
-	size_option = '--sizespec ' + size
+	size_option = '--sizespec ' + size + ' --sizefact 10'
+	outfile_option = '--outfile ' + prefix
+	kernels_option = '--kernels ' + kernels
+
+	command_string = " ".join(['./bin/raja-perf.exe', passes_option, size_option, outfile_option, kernels_option])
+
+	return command_string
+
+def pointwise_eval():
+	num_passes = 10
+	size = '10'
+	kernels = "ENERGY PRESSURE GEN_LIN_RECUR"
+	prefix = 'Pointwise'
+
+	passes_option = '--npasses ' + str(num_passes)
+	size_option = '--sizefact ' + size
 	outfile_option = '--outfile ' + prefix
 	kernels_option = '--kernels ' + kernels
 
@@ -19,3 +34,5 @@ def stencil_eval():
 	return command_string
 
 print(stencil_eval())
+print(pointwise_eval())
+
